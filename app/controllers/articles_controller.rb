d962dba52a58eb66a1ed7f65889fa2fa414119f4
Christian Rojas <christiandiegor@gmail.com>
2015-04-22 22:35:05 -0300

    creación de controlador de articles y vistas

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,33 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(title: params[:article][:title], 
+								body: params[:article][:body])
+
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	def update
+	end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
-  def index
-    
+  def index 
   end
 end
      
