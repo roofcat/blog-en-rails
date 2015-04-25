b1cb8f0375f94fc175385458b57b54dd10dc434c
Christian Rojas <christiandiegor@gmail.com>
2015-04-25 14:35:09 -0300

    se termino crud articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,5 +33,11 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render :edit
+		end
 	end
 end
