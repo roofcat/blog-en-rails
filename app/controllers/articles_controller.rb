052aec29e37fb13b73918153c203f69ef05f3ba4
Christian Rojas <christiandiegor@gmail.com>
2015-04-27 12:24:00 -0300

    se modifico creacion de articles relacionado con usuarios

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,8 +16,8 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(title: params[:article][:title], 
-								body: params[:article][:body])
+		#@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		@article = current_user.articles.new(article_params)
 
 		if @article.save
 			redirect_to @article
@@ -39,5 +39,10 @@ class ArticlesController < ApplicationController
 		else
 			render :edit
 		end
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :body)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427123127) do
+ActiveRecord::Schema.define(version: 20150427135402) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",        limit: 255
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150427123127) do
     t.integer  "visits_count", limit: 4
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "user_id",      limit: 4
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -41,4 +44,5 @@ ActiveRecord::Schema.define(version: 20150427123127) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "articles", "users"
 end
