f70a11ea7043073fc926dab4932cbe4ebc820b49
Christian Rojas <christiandiegor@gmail.com>
2015-06-22 21:09:44 -0300

    creacion de scaffold de categorias

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611014019) do
+ActiveRecord::Schema.define(version: 20150622234204) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",              limit: 255
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150611014019) do
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "color",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
